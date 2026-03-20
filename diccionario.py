60d72939499fe87b99a92e6d37f0d9acc2983d01
Adri-88 <delvalleadriana385@gmail.com>
2026-03-20 07:57:34 -0500

    Add files via upload

---
+++ diccionario.py
@@ -0,0 +1,8 @@
+persona = {" nombre": "adriana", "edad": 18, "ciudad": "santa marta"}
+
+for clave in persona:
+    print(clave)    
+
+    for clave, valor in persona.items():
+        print(clave + ": " + str(valor))
+        
